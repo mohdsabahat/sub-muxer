c78d8ff6b31269578705f35c75c082cf23b38018
dramastore <64647629+dramastore@users.noreply.github.com>
2021-08-30 22:30:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    APP_ID = os.environ.get('APP_ID', None)
-    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('1943845080:AAHLsLZi6CM_0izDkfFuPtM2MwVRNU5euEs', None)
+    APP_ID = os.environ.get( 7069146 , None)
+    API_HASH = os.environ.get(' 7f98778e1b3770043d2392c12185a686 ', None)
 
     #comma seperated user id of users who are allowed to use
-    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1098504493').split(',')]
+    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('1098504493', ' 7069146 ').split(',')]
 
     DOWNLOAD_DIR = 'downloads'
